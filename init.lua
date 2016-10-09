77d15e7ef9da9c4bccda3a8ff1ae11121ec7e844
zander999 <zandertmarshall@gmail.com>
2016-10-10 09:59:21 +1000

    add taliant reversed

--- init.lua
+++ init.lua
@@ -7,9 +7,23 @@ minetest.register_craftitem("taillant:taillant", {
 minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
 	if puncher:get_wielded_item():get_name() == "taillant:taillant"
 	and node.name == "default:stone" then
-			minetest.remove_node(pos)
-		 	minetest.add_node(pos, {name="default:cobble"})
-			puncher:get_inventory():add_item('main', "default:cobble")
+		minetest.set_node(pos, {name="default:cobble"})
+		puncher:get_inventory():add_item('main', "default:gravel")
+		end
+	end
+)
+
+
+minetest.register_craftitem("taillant:taillant_reverse", {
+    description = "The Taillant(in reverse)",
+    inventory_image = "stone_extractor.png"
+})
+
+minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
+	if puncher:get_wielded_item():get_name() == "taillant:taillant_reverse"
+	and node.name == "default:cobble" then
+		minetest.set_node(pos, {name="default:stone"})
+		puncher:get_inventory():remove_item('main', "default:gravel")
 		end
 	end
 )
