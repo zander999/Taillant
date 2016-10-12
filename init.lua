cee620ba6fa22ac4e57d79d83ec746873fd06c0c
Tai Kedzierski <dch.tai@gmail.com>
2016-10-12 23:56:21 +0100

    Streamlined code
    * only need 2 tools - no need for a separate tool (unless you really, really wanted it)
    * only register one handler - no need to manually register 2 for the same mod
    * reorganized
    * reverse taillant - check we actually have gravel first

--- init.lua
+++ init.lua
@@ -1,43 +1,42 @@
+-- Suporting functions
+
+local hasitem = function(inventory,itemstring)
+	for idx,x in pairs(inventory:get_list("main") ) do
+		if x:get_name() == itemstring then
+			return ture
+		end
+	end
+	return false
+end
+
+-- Item defintions
 
 minetest.register_craftitem("taillant:taillant", {
     description = "The Taillant",
     inventory_image = "stone_extractor.png"
 })
 
-minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
-	if puncher:get_wielded_item():get_name() == "taillant:taillant"
-	and node.name == "default:stone" then
-		minetest.set_node(pos, {name="default:cobble"})
-		puncher:get_inventory():add_item('main', "default:gravel")
-		end
-	end
-)
-
-
 minetest.register_craftitem("taillant:taillant_reverse", {
-    description = "The Taillant(in reverse)",
+    description = "The Reverse Taillant",
     inventory_image = "stone_extractor_reverse.png"
 })
 
+-- Effects
+
 minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
-	if puncher:get_wielded_item():get_name() == "taillant:taillant_reverse"
-	and node.name == "default:cobble" then
+	if puncher:get_wielded_item():get_name() == "taillant:taillant" then
+		if node.name == "default:stone" then
+			minetest.set_node(pos, {name="default:cobble"})
+			puncher:get_inventory():add_item('main', "default:gravel")
+		elseif node.name == "default:cobble" then
+			minetest.remove_node(pos, {name="default:cobble"})
+			puncher:get_inventory():add_item('main', "default:gravel")
+		end
+	elseif puncher:get_wielded_item():get_name() == "taillant:taillant_reverse"
+	and node.name == "default:cobble"
+	and hasitem(puncher:get_inventory(),"default:gravel") then
 		minetest.set_node(pos, {name="default:stone"})
 		puncher:get_inventory():remove_item('main', "default:gravel")
-		end
 	end
-)
+end)
 
-minetest.register_craftitem("taillant:taillant_smash", {
-    description = "The Taillant Destroyer",
-    inventory_image = "stone_extractor_blase.png"
-})
-
-minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
-	if puncher:get_wielded_item():get_name() == "taillant:taillant_smash"
-	and node.name == "default:cobble" then
-		minetest.remove_node(pos, {name="default:cobble"})
-		puncher:get_inventory():add_item('main', "default:gravel")
-		end
-	end
-)
